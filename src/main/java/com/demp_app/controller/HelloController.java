8d6596882a694487d5755051947e9fa6dfcdac45
Suraj746 <pradhansuraj404@gmail.com>
2025-05-18 15:53:44 +0530

    added the testing code for docker

---
+++ src/main/java/com/demp_app/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.demp_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Nitish is Great";
+				}
+
+}
